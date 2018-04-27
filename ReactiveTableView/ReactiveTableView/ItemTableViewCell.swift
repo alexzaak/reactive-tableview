75704d5368f7fb591b9536fd74f19fe33c15232f
Alexander Zaak <zaak@weloveapps.de>
2018-04-27 17:51:01 +0200

    add gitignore

---
+++ ReactiveTableView/ReactiveTableView/ItemTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ItemTableViewCell.swift
+//  ReactiveTableView
+//
+//  Created by Alexander Zaak on 27.04.18.
+//  Copyright © 2018 Alexander Zaak. All rights reserved.
+//
+
+import UIKit
+
+class ItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
